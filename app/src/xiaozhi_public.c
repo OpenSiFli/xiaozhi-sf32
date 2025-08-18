7e198b818831b1bd163538a7e815c850a1737f06
zhenpengfu <zhenpengfu@sifli.com>
2025-08-18 09:57:47 +0800

    添加待机界面以及获取准确天气和时间

--- app/src/xiaozhi_public.c
+++ app/src/xiaozhi_public.c
@@ -418,9 +418,11 @@ static int sleep_countdown = 3;
 static lv_timer_t *sleep_timer = NULL;
 static volatile int g_sleep_countdown_active = 0; // 休眠倒计时标志
 
-
+extern rt_timer_t update_time_ui_timer;
+extern rt_timer_t update_weather_ui_timer;
 static void sleep_countdown_cb(lv_timer_t *timer)
 {
+    
     if (sleep_label && sleep_countdown > 0)
     {
         char num[2] = {0};
@@ -431,6 +433,16 @@ static void sleep_countdown_cb(lv_timer_t *timer)
     }
     else
     {
+        if(update_time_ui_timer)
+        {
+            rt_timer_stop(update_time_ui_timer);//睡眠停止ui更新
+        }
+        
+        if(update_weather_ui_timer)
+        {
+            rt_timer_stop(update_weather_ui_timer);
+        }
+
         lv_timer_delete(sleep_timer);
         sleep_timer = NULL;
         g_sleep_countdown_active = 0; // 倒计时结束，清除标志
